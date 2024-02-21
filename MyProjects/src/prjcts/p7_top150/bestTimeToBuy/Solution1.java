e841a349ee2765694bcffa8f1eb4e1d02624a705
Nicat Qurbanov <nicat.qurbanov97@gmail.com>
2024-02-21 18:59:05 +0400

    21.02.24

--- MyProjects/src/prjcts/p7_top150/bestTimeToBuy/Solution1.java
+++ MyProjects/src/prjcts/p7_top150/bestTimeToBuy/Solution1.java
@@ -1,4 +1,24 @@
 package prjcts.p7_top150.bestTimeToBuy;
 
 public class Solution1 {
+    public static void main(String[] args) {
+
+        int[] a = new int[] {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(a));
+
+    }
+
+    public static int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+
+        for (int price : prices) {
+            if (price <= min) min = price;
+
+            profit = Math.max (profit, (price - min));
+        }
+
+        return profit;
+    }
 }
